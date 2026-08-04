b33582c50be74fb4a6da6380ee13e64947aff08e
guckoishiii <jirayoot.krt@gmail.com>
2026-08-04 20:56:07 +0700

    Update GOAL77 from Admin

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const DATA = {
   brand: "กลุ่มไก่ใต้ GOAL77 โปรโมชั่นคืนยอดเสีย",
-  stadium: "",
+  stadium: "สนามชนไก่มะม่วงปลายแขน รายการพิเศษ วัยรุ่นรางรถไฟ&วัยรุ่นท่าหลา",
   pairs: 22,
-  status: 3,
+  status: 1,
   image: "og-image.jpg",
   logo: "logo.png",
   line: "https://lin.ee/Lklfa53"
