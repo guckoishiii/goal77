62d1f21d79ca47dd59c1effb661cd216d19ad4ff
guckoishiii <jirayoot.krt@gmail.com>
2026-08-05 12:30:53 +0700

    Update GOAL77 from Admin

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const DATA = {
   brand: "กลุ่มไก่ใต้ GOAL77 โปรโมชั่นคืนยอดเสีย",
   stadium: "🐓 05-08-69 สนามชนไก่ลิปะใหญ่ เกาะสมุย 20คู่ชน รายการธรรมดา คู่เอก 660,000",
   pairs: 20,
-  status: 2,
+  status: 3,
   image: "og-image.jpg",
   logo: "logo.png",
   line: "https://lin.ee/Lklfa53"
